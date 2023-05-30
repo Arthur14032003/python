a93bfd7542c87ba96dcc72cf1dd92ad55c89f042
Arthur14032003 <arthur.tremeau@institutsolacroup.com>
2023-05-30 11:13:47 +0200

    modifications du code

--- fonctionsprédéfinies.py
+++ fonctionsprédéfinies.py
@@ -1,11 +1,8 @@
-i, list = 1, []
-# i est un booléen true
-
-while i:
-    valeurInput = input("Veuillez entrer une valeur : ")
-    if valeurInput != "":  # tant que l'utilisateur ne rentre pas d'entrée
-        list.append(int(valeurInput))  # on entre l'input dans la liste
-    else:
-        i = 0  # sinon, i vaut false
+list = []
+while True:
+    messages = input("entre une valeur : ")
+    if messages == "":
+        break
+    list.append(int(messages))
 
 print(list)
